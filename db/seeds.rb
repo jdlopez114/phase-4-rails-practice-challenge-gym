ffe197edd9540ee5ba7d13691e573dd3b7d7eb99
Josiel <jdlopez114@gmail.com>
2023-02-05 00:23:27 -0500

    start

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+Gym.destroy_all
+Client.destroy_all
+Membership.destroy_all
+
+puts "🐕 Seeding data..."
+
 la = Gym.create(name: "L.A. Fitness", address: "Suite 981 1795 Leida Fords, Nataliemouth, TN 14377-4979")
 planet = Gym.create(name: "Planet Fitness", address: "7768 David Avenue, Greenside, PA 67475-3655")
 
@@ -11,3 +17,5 @@ Membership.create(gym_id: la.id, client_id: dorris.id, charge: 40)
 Membership.create(gym_id: la.id, client_id: bernie.id, charge: 40)
 Membership.create(gym_id: planet.id, client_id: cassie.id, charge: 50)
 Membership.create(gym_id: planet.id, client_id: manie.id, charge: 60)
+
+puts "✅ Done seeding!"
